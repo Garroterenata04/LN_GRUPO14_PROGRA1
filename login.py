7d1ad3dd8e3de83838271bdf9a54ea49aced5bd0
Garroterenata04 <rgarrote@uade.edu.ar>
2025-09-01 12:59:11 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
+def login():
+    nombre = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+    correct_nombre = "admin"
+    correct_contra = "1234"
 
+    if nombre == correct_nombre and contraseña == correct_contra:
+        print(f"\nEl inicio de sesión fue exitoso. Bienvenido, {nombre}.")
+    else:
+        print("\nEl nombre de usuario o contraseña ingresados son incorrectos. Intente nuevamente.")
