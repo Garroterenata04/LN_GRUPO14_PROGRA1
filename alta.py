26c912a5a2ff61dbb8887c2318d944548d07bde9
Garroterenata04 <rgarrote@uade.edu.ar>
2025-09-01 13:50:19 -0300

    Update alta.py

--- alta.py
+++ alta.py
@@ -1,1 +1,26 @@
+def add_expense(movimientos, categorias):
+    id_mov = len(movimientos)
+
+    fecha = input("Ingrese la fecha (MM/DD/YYYY): ")
+    monto = input("Ingrese el monto: ")
+    movimientos.append([monto])
+    tipo_mov = input("Ingrese el tipo de movimiento (Gasto/Transferencia): ")
+
+    print("\nSeleccione la categoría:")
+    for cat in categorias[1:]:
+        print(f"{cat[0]} - {cat[1]} ({cat[2]})")
+
+    cat_id = int(input("Ingrese el número de categoría: "))
+
+    categoria = "Desconocido"
+    for cat in categorias[1:]:
+        if cat[0] == cat_id:
+            categoria = cat[1]
+
+    descripcion = input("Ingrese la descripción: ")
+    cuenta_origen = input("Ingrese la cuenta origen (o '-' si no aplica): ")
+    cuenta_destino = input("Ingrese la cuenta destino (o '-' si no aplica): ")
+    movimientos.append([id_mov, fecha, monto, tipo_mov, categoria, descripcion, cuenta_origen, cuenta_destino])
+
+    print("\n Movimiento agregado correctamente!\n")
 
